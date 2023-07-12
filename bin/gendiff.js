e1580cb31e994db1527881a0b248e49f72a79f08
bearBenjamin <ilhakonev@yandex.ru>
2023-07-12 22:30:51 +0300

    поиск ошибки

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,10 +10,21 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
-  .arguments('<filepath1> <filepath2> [formateName]')
+  .arguments('<filepath1> <filepath2> [formatName]')
   .option('-f, --format <type>', 'add the specified type of format', 'stylish')
-  .action((filepath1, filepath2, formatName = 'stylish') => {
-    console.log(getDiff(filepath1, filepath2, formatName));
+  .action((filepath1, filepath2, formatName) => {
+    if (formatName === undefined) {
+      console.log(getDiff(filepath1, filepath2, program.opts().format));
+    }
+    if (formatName === 'stylish') {
+      console.log(getDiff(filepath1, filepath2, 'stylish'));
+    }
+    if (formatName === 'plain') {
+      console.log(getDiff(filepath1, filepath2, 'plain'));
+    }
+    if (formatName === 'json') {
+      console.log(getDiff(filepath1, filepath2, 'json'));
+    }
   });
 
 program.parse();
