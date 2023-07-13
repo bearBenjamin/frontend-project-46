32704535ca2e1debec30cefd30dda979b378c674
bearBenjamin <ilhakonev@yandex.ru>
2023-07-13 15:10:38 +0300

    трах-тибидох, ошибка в тестах Хекслета пропади

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,15 +3,17 @@
 import _ from 'lodash';
 
 const indentsFormater = (item) => {
-  const replacer = '..';
-  const spaceCount = 1;
+  const replacer = '  ';
+  const spaceCount = 2;
   const indent = spaceCount * item;
   const indentCloseBrace = indent - spaceCount;
   const indents = {
-    numberIndents: replacer.repeat(indent),
+    numberIndents: replacer.repeat(indent - 1),
     closeBrace: replacer.repeat(indentCloseBrace),
   };
-  const { numberIndents, closeBrace } = indents;
+  const {
+    numberIndents, closeBrace,
+  } = indents;
   return [numberIndents, closeBrace];
 };
 
@@ -21,10 +23,10 @@ const stringify = (value, depth) => {
   if (!_.isObject(value)) return `${value}`;
 
   const objectProperties = Object.keys(value).map((key) => {
-    if (_.isObject(value)) return `${numberIndents}    ${key}: ${stringify(value[key], depth + 1)}`;
-    return `${numberIndents}    ${key}: ${value[key]}`;
+    if (_.isObject(value)) return `${numberIndents}  ${key}: ${stringify(value[key], depth + 1)}`;
+    // return `${numberIndents} ${key}: ${value[key]}`; не понятно, нужна эта строка или не нужна???
   });
-  return `{\n${objectProperties.join('\n')}\n${closeBrace}|-|-}`;
+  return `{\n${objectProperties.join('\n')}\n${closeBrace}}`;
 };
 
 const getDiffTreeObject = (treeObject, depth = 1) => {
@@ -32,12 +34,12 @@ const getDiffTreeObject = (treeObject, depth = 1) => {
 
   const keys = treeObject.map((key) => {
     if (key.type === 'node') return `${numberIndents}  ${key.key}: ${getDiffTreeObject(key.children, depth + 1)}`;
-    if (key.type === 'delete') return `${numberIndents}  - ${key.key}: ${stringify(key.value1, depth + 1)}`;
-    if (key.type === 'unchanged') return `${numberIndents}    ${key.key}: ${stringify(key.value1, depth + 1)}`;
-    if (key.type === 'changed') return `${numberIndents}  - ${key.key}: ${stringify(key.value1, depth + 1)}\n${numberIndents}  + ${key.key}: ${stringify(key.value2, depth + 1)}`;
-    if (key.type === 'added') return `${numberIndents}  + ${key.key}: ${stringify(key.value2, depth + 1)}`;
+    if (key.type === 'delete') return `${numberIndents}- ${key.key}: ${stringify(key.value1, depth + 1)}`;
+    if (key.type === 'unchanged') return `${numberIndents}  ${key.key}: ${stringify(key.value1, depth + 1)}`;
+    if (key.type === 'changed') return `${numberIndents}- ${key.key}: ${stringify(key.value1, depth + 1)}\n${numberIndents}+ ${key.key}: ${stringify(key.value2, depth + 1)}`;
+    if (key.type === 'added') return `${numberIndents}+ ${key.key}: ${stringify(key.value2, depth + 1)}`;
   });
-  return `{\n${keys.join('\n')}\n${closeBrace}|-|-}`;
+  return `{\n${keys.join('\n')}\n${closeBrace}}`;
 };
 
 export default getDiffTreeObject;
