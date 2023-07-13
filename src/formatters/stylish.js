306397c166a481ea249f0c795b06ec16529c4a42
bearBenjamin <ilhakonev@yandex.ru>
2023-07-13 10:36:11 +0300

    угадай количество отступов -3

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,7 +3,7 @@
 import _ from 'lodash';
 
 const indentsFormater = (item) => {
-  const replacer = '...';
+  const replacer = '....';
   const spaceCount = 1;
   const indent = spaceCount * item;
   const indentCloseBrace = indent - spaceCount;
@@ -21,8 +21,8 @@ const stringify = (value, depth) => {
   if (!_.isObject(value)) return `${value}`;
 
   const objectProperties = Object.keys(value).map((key) => {
-    if (_.isObject(value)) return `${numberIndents}${key}: ${stringify(value[key], depth + 1)}`;
-    return `${numberIndents}${key}: ${value[key]}`;
+    if (_.isObject(value)) return `${numberIndents} ${key}: ${stringify(value[key], depth + 1)}`;
+    return `${numberIndents} ${key}: ${value[key]}`;
   });
   return `{\n${objectProperties.join('\n')}\n${closeBrace}}`;
 };
@@ -31,7 +31,7 @@ const getDiffTreeObject = (treeObject, depth = 1) => {
   const [numberIndents, closeBrace] = indentsFormater(depth);
 
   const keys = treeObject.map((key) => {
-    if (key.type === 'node') return `${numberIndents}${key.key}: ${getDiffTreeObject(key.children, depth + 1)}`;
+    if (key.type === 'node') return `${numberIndents}  ${key.key}: ${getDiffTreeObject(key.children, depth + 1)}`;
     if (key.type === 'delete') return `${numberIndents}- ${key.key}: ${stringify(key.value1, depth + 1)}`;
     if (key.type === 'unchanged') return `${numberIndents}  ${key.key}: ${stringify(key.value1, depth + 1)}`;
     if (key.type === 'changed') return `${numberIndents}- ${key.key}: ${stringify(key.value1, depth + 1)}\n${numberIndents}+ ${key.key}: ${stringify(key.value2, depth + 1)}`;
