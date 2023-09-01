474a035469fb0d508ca6356a7be227846f7a363e
bearBenjamin <ilhakonev@yandex.ru>
2023-09-01 17:06:08 +0300

    правка после четвертой проверки

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,11 +2,27 @@ import getJsonFormat from './json.js';
 import getPlainFormat from './plain.js';
 import getStylishFormat from './stylish.js';
 
-const format = (formatName, diff) => {
+/* const format = (formatName, diff) => {
   if (formatName === 'stylish') return getStylishFormat(diff);
   if (formatName === 'plain') return getPlainFormat(diff);
   if (formatName === 'json') return getJsonFormat(diff);
   return 'format not found';
+}; */
+
+const format = (formatName, diff) => {
+  switch (formatName) {
+    case 'stylish': {
+      return getStylishFormat(diff);
+    }
+    case 'plain': {
+      return getPlainFormat(diff);
+    }
+    case 'json': {
+      return getJsonFormat(diff);
+    }
+    default:
+      throw new Error(`${formatName} not supported`);
+  }
 };
 
 export default format;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,10 +2,11 @@
 /* eslint-disable array-callback-return */
 import _ from 'lodash';
 
-const indentsFormater = (item) => {
-  const replacer = '  ';
-  const spaceCount = 2;
-  const indent = spaceCount * item;
+const replacer = '  ';
+const spaceCount = 2;
+
+const retreat = (depth) => {
+  const indent = spaceCount * depth;
   const indentCloseBrace = indent - spaceCount;
   const indents = {
     numberIndents: replacer.repeat(indent - 1),
@@ -18,26 +19,39 @@ const indentsFormater = (item) => {
 };
 
 const stringify = (value, depth) => {
-  const [numberIndents, closeBrace] = indentsFormater(depth);
+  const [numberIndents, closeBrace] = retreat(depth);
 
   if (!_.isObject(value)) return `${value}`;
 
-  const objectProperties = Object.keys(value).map((key) => {
-    if (_.isObject(value)) return `${numberIndents}  ${key}: ${stringify(value[key], depth + 1)}`;
-    // return `${numberIndents} ${key}: ${value[key]}`; не понятно, нужна эта строка или не нужна???
-  });
+  const objectProperties = Object.keys(value).map((key) => `${numberIndents}  ${key}: ${stringify(value[key], depth + 1)}`);
   return `{\n${objectProperties.join('\n')}\n${closeBrace}}`;
 };
 
 const getStylishFormat = (diff, depth = 1) => {
-  const [numberIndents, closeBrace] = indentsFormater(depth);
+  const [numberIndents, closeBrace] = retreat(depth);
 
   const keys = diff.map((key) => {
-    if (key.type === 'node') return `${numberIndents}  ${key.key}: ${getStylishFormat(key.children, depth + 1)}`;
-    if (key.type === 'delete') return `${numberIndents}- ${key.key}: ${stringify(key.value, depth + 1)}`;
-    if (key.type === 'unchanged') return `${numberIndents}  ${key.key}: ${stringify(key.value, depth + 1)}`;
-    if (key.type === 'changed') return `${numberIndents}- ${key.key}: ${stringify(key.value1, depth + 1)}\n${numberIndents}+ ${key.key}: ${stringify(key.value2, depth + 1)}`;
-    if (key.type === 'added') return `${numberIndents}+ ${key.key}: ${stringify(key.value, depth + 1)}`;
+    switch (key.type) {
+      case 'node': {
+        return `${numberIndents}  ${key.key}: ${getStylishFormat(key.children, depth + 1)}`;
+      }
+      case 'delete': {
+        return `${numberIndents}- ${key.key}: ${stringify(key.value, depth + 1)}`;
+      }
+      case 'unchanged': {
+        return `${numberIndents}  ${key.key}: ${stringify(key.value, depth + 1)}`;
+      }
+      case 'changed': {
+        const line1 = `${numberIndents}- ${key.key}: ${stringify(key.value1, depth + 1)}`;
+        const line2 = `${numberIndents}+ ${key.key}: ${stringify(key.value2, depth + 1)}`;
+        return `${line1}\n${line2}`;
+      }
+      case 'added': {
+        return `${numberIndents}+ ${key.key}: ${stringify(key.value, depth + 1)}`;
+      }
+      default:
+        return null;
+    }
   });
   return `{\n${keys.join('\n')}\n${closeBrace}}`;
 };
